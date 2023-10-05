87f57c851bad31e680154cccf1fb53e1a71d8687
José Luis González <joseluisgonsan@gmail.com>
2023-10-05 09:44:52 +0200

    Paginación, filtro, orden y búsqueda

--- paginacion/src/resources/countries-languages/countries-languages.controller.ts
+++ paginacion/src/resources/countries-languages/countries-languages.controller.ts
@@ -1,15 +1,46 @@
-import { Controller, Get } from '@nestjs/common'
-import { CountriesLanguagesService } from './countries-languages.service'
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Logger,
+  Query,
+} from '@nestjs/common'
+import {
+  CountriesLanguagesService,
+  CountryLanguageFilter,
+  CountryLanguageOrder,
+} from './countries-languages.service'
 
 @Controller('countries-languages')
 export class CountriesLanguagesController {
+  private logger = new Logger(CountriesLanguagesController.name)
+
   constructor(
     private readonly countriesLanguagesService: CountriesLanguagesService,
   ) {}
 
   // Aquí irán los métodos del controlador
   @Get()
-  findAll() {
-    return this.countriesLanguagesService.findAll()
+  findAll(
+    @Query('page', new DefaultValuePipe(0))
+    page: number = 1,
+    @Query('pageSize', new DefaultValuePipe(0))
+    pageSize: number = 20,
+    @Query('filter', new DefaultValuePipe('CountryCode'))
+    filter: CountryLanguageFilter = 'CountryCode',
+    @Query('order', new DefaultValuePipe('asc'))
+    order: CountryLanguageOrder = 'asc',
+    @Query('search', new DefaultValuePipe('')) search: string = '',
+  ) {
+    // this.logger.log(`page: ${page}, pageSize: ${pageSize}`)
+    if (pageSize)
+      return this.countriesLanguagesService.findAllPaginated(
+        page,
+        pageSize,
+        filter,
+        order,
+        search,
+      )
+    else return this.countriesLanguagesService.findAll()
   }
 }
--- paginacion/src/resources/countries-languages/countries-languages.service.ts
+++ paginacion/src/resources/countries-languages/countries-languages.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { PaginateModel } from 'mongoose' // has necesitado importar el esquema en el createFactory del esquema
 import {
@@ -6,8 +6,18 @@ import {
   CountryLanguageDocument,
 } from './schemas/countries-language.schema'
 
+// Aquí irían los tipos de datos para los filtros y la ordenación
+export type CountryLanguageFilter =
+  | 'CountryCode'
+  | 'Language'
+  | 'IsOfficial'
+  | 'Percentage'
+export type CountryLanguageOrder = 'asc' | 'desc'
+
 @Injectable()
 export class CountriesLanguagesService {
+  private logger = new Logger(CountriesLanguagesService.name)
+
   constructor(
     @InjectModel(CountryLanguage.name)
     private countryLanguageModel: PaginateModel<CountryLanguageDocument>,
@@ -15,5 +25,37 @@ export class CountriesLanguagesService {
 
   async findAll(): Promise<CountryLanguage[]> {
     return await this.countryLanguageModel.find().exec()
+  }
+
+  async findAllPaginated(
+    page: number,
+    pageSize: number,
+    filter: CountryLanguageFilter,
+    order: CountryLanguageOrder,
+    search: string,
+  ) {
+    this.logger.log(
+      `page: ${page}, pageSize: ${pageSize}, filter: ${filter}, order: ${order}, search: ${search}`,
+    )
+    // Aquí iría la query de búsqueda y filtrado
+    const query = {
+      [filter]: {
+        $regex: `.*${search}.*`, // para que busque en cualquier parte del campo
+        $options: 'i', // para que no distinga entre mayúsculas y minúsculas
+      },
+    }
+    // Aquí iría la query de ordenación y paginación
+    const options = {
+      page,
+      limit: pageSize,
+      sort: { [filter]: order },
+      collection: 'es_ES', // para que use la configuración de idioma de España
+    }
+    // lanzamos la operación de búsqueda y paginación
+    // si no hay filtro, query será un objeto vacío, y no puede ser Percentage
+    return await this.countryLanguageModel.paginate(
+      filter !== 'Percentage' ? query : {},
+      options,
+    )
   }
 }
